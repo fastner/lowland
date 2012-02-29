4ba67c329a3a449c715b881983fbf94bee8cf05b
Sebastian Fastner <mail@sebastianfastner.de>
2012-02-29 17:21:55 +0100

    Add event check to addListener and addListenerOnce, remove it from fireEvent

--- src/base/Events.js
+++ src/base/Events.js
@@ -4,23 +4,29 @@ core.Class("lowland.base.Events", {
   members : {
     fireEvent : function(type, value, old) {
       var events = core.Class.getEvents(this.constructor);
-      var cls = events[type];
-      if (core.Env.getValue("debug")) {
-        if (!cls || !core.Class.isClass(cls)) {
-          //throw new Error("Class " + this.constructor + " has no event " + type);
-          console.warn("Class " + this.constructor + " has no event " + type);
-        }
-      }
-      if (cls) {
-        lowland.events.EventManager.fireEvent(this, type, cls, [value, old]);
-      }
+      var cls = events[type] || lowland.events.Event;
+      lowland.events.EventManager.fireEvent(this, type, cls, [value, old]);
     },
     
     addListener : function(event, callback, context) {
+      if (core.Env.getValue("debug")) {
+        var cls = core.Class.getEvents(this.constructor)[event];
+        if (!cls || !core.Class.isClass(cls)) {
+          //throw new Error("Class " + this.constructor + " has no event " + type);
+          console.warn("Class " + this.constructor + " has no event " + event + " to listen on");
+        }
+      }
       lowland.events.EventManager.addListener(this, event, callback, context);
     },
     
     addListenerOnce : function(event, callback, context) {
+      if (core.Env.getValue("debug")) {
+        var cls = core.Class.getEvents(this.constructor)[event];
+        if (!cls || !core.Class.isClass(cls)) {
+          //throw new Error("Class " + this.constructor + " has no event " + type);
+          console.warn("Class " + this.constructor + " has no event " + event + " to listen on");
+        }
+      }
       var self = this;
       var cb = function() {
         lowland.events.EventManager.removeListener(self, event, cb, context);
