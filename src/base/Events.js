5b73a0679bab21566822e8063254cb6b58cea898
Sebastian Fastner <mail@sebastianfastner.de>
2012-08-02 14:50:12 +0200

    Add capture phase events listening for addNativeEvent

--- src/base/Events.js
+++ src/base/Events.js
@@ -97,7 +97,7 @@ core.Class("lowland.base.Events", {
      * Add event listener on @event {String} to @element {Element}. If the event is fired the
      * @callback {Function} is executed in @context {var?null}.
      */
-    addNativeListener : function(element, event, callback, context) {
+    addNativeListener : function(element, event, callback, context, capture) {
       var boundCallback;
       
       if (!context) {
@@ -105,19 +105,19 @@ core.Class("lowland.base.Events", {
       }
       
       boundCallback = lowland.events.BindManager.bind(callback, context);
-      lowland.bom.Events.listen(element, event, boundCallback);
+      lowland.bom.Events.listen(element, event, boundCallback, capture);
     },
     
     /**
      * Remove event listener on @event {String} from @element {Element} that has @callback {Function?null} and @context {var?null} given.
      */
-    removeNativeListener : function(element, event, callback, context) {
+    removeNativeListener : function(element, event, callback, context, capture) {
       if (!context) {
         context = this;
       }
       
       var boundCallback = lowland.events.BindManager.bind(callback, context);
-      lowland.bom.Events.unlisten(element, event, boundCallback);
+      lowland.bom.Events.unlisten(element, event, boundCallback, capture);
     }
   }
 });
