c7be092ed07b70591bc695c6cfc0e5ac5237004d
Sebastian Fastner <mail@sebastianfastner.de>
2012-08-08 11:21:03 +0200

    Fix style test

--- test/source/class/lowtest/bom/Style.js
+++ test/source/class/lowtest/bom/Style.js
@@ -6,11 +6,11 @@ core.Module("lowtest.bom.Style", {
     test("test for class", function() {
       var css = "color:'red';";
       
-      document.head = {
-        appendChild : function(element) {
-          ok(false,  "add css text");
-        }
-      };
+      var oldAppendChild = document.head.appendChild;
+      document.head.appendChild = function(element) {
+          document.head.appendChild = oldAppendChild;
+          ok(true,  "add css text");
+      }
       
       lowland.bom.Style.addStyleText(css);
     });
