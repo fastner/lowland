bc4a17949fba80a558b434f814d2123b2c2a3161
Sebastian Fastner <mail@sebastianfastner.de>
2013-02-08 14:10:56 +0100

    Move native event listener to own mixin

---
+++ src/base/NativeEvents.js
@@ -0,0 +1,37 @@
+/**
+ * Generic non bubling event system for core classes.
+ */
+(function() {
+	
+core.Class("lowland.base.NativeEvents", {
+	members : {
+		/**
+		 * Add event listener on @event {String} to @element {Element}. If the event is fired the
+		 * @callback {Function} is executed in @context {var?null}.
+		 */
+		addNativeListener : function(element, event, callback, context, capture) {
+			var boundCallback;
+			
+			if (!context) {
+				context = this;
+			}
+			
+			boundCallback = lowland.events.BindManager.bind(callback, context);
+			lowland.bom.Events.listen(element, event, boundCallback, capture);
+		},
+		
+		/**
+		 * Remove event listener on @event {String} from @element {Element} that has @callback {Function?null} and @context {var?null} given.
+		 */
+		removeNativeListener : function(element, event, callback, context, capture) {
+			if (!context) {
+				context = this;
+			}
+			
+			var boundCallback = lowland.events.BindManager.bind(callback, context);
+			lowland.bom.Events.unlisten(element, event, boundCallback, capture);
+		}
+	}
+});
+
+})();
