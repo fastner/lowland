db300bb02b9a7071fea68390f210ebb4b6ea3a67
Sebastian Fastner <mail@sebastianfastner.de>
2013-08-13 15:19:15 +0200

    Fix ObjectURL in browsers that don't support it

--- src/bom/Binary.js
+++ src/bom/Binary.js
@@ -1,8 +1,26 @@
 (function() {
 
 	var URL = window.URL || window.webkitURL;
+	var supported = true;
+	if (!URL) {
+		supported = false;
+		URL = {
+			createObjectURL : function() {
+				if (jasy.Env.isSet("debug")) {
+					console.error("Browser don't support ObjectURL");
+				}
+			},
+			revokeObjectURL : function() {
+				if (jasy.Env.isSet("debug")) {
+					console.error("Browser don't support ObjectURL");
+				}
+			}
+		};
+	}
 
 	core.Module("lowland.bom.Binary", {
+		isSupported : supported,
+
 		createUrl : URL.createObjectURL,
 
 		revokeUrl : URL.revokeObjectURL,
